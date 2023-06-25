d7ac8f09cc23864f38dd15c7e47f32ffe9d03328
Retro <44505837+dankmolot@users.noreply.github.com>
2023-06-25 18:21:45 +0300

    A little change to watchdog architecture

--- source/watchdog.hpp
+++ source/watchdog.hpp
@@ -9,6 +9,8 @@
 #include <queue>
 #include <mutex>
 #include <tier0/platform.h>
+#include <efsw/efsw.hpp>
+#include <tier0/dbg.h>
 
 namespace efsw {
     class FileWatcher;
@@ -16,12 +18,16 @@ namespace efsw {
 }
 
 namespace MoonLoader {
-    class WatchdogListener;
+    class WatchdogListener : public efsw::FileWatchListener {
+    public:
+        void handleFileAction(efsw::WatchID watchid, const std::string& dir,
+            const std::string& filename, efsw::Action action,
+            std::string oldFilename) override;
+    };
 
     class Watchdog {
-        efsw::FileWatcher* m_Watcher;
-        WatchdogListener* m_WatchdogListener;
-
+        std::unique_ptr<efsw::FileWatcher> m_Watcher;
+        std::unique_ptr<WatchdogListener> m_WatchdogListener;
         std::unordered_map<std::string, efsw::WatchID> m_WatchIDs;
         std::unordered_set<std::string> m_WatchedFiles;
 
@@ -31,7 +37,6 @@ namespace MoonLoader {
 
     public:
         Watchdog();
-        ~Watchdog();
 
         inline bool IsFileWatched(const std::string& path) { return m_WatchedFiles.find(path) != m_WatchedFiles.end(); }
         inline bool IsDirectoryWatched(const std::string& path) { return m_WatchIDs.find(path) != m_WatchIDs.end(); }
